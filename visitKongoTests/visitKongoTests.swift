56a5b9d4a1b2bbd5b5d3fc52b7f1278130fb194d
ethbergmuzola <ethbergmuzola500@gmail.com>
2025-04-09 15:40:31 +0100

    Feat:Initialisation du projet

---
+++ visitKongoTests/visitKongoTests.swift
@@ -0,0 +1,17 @@
+//
+//  visitKongoTests.swift
+//  visitKongoTests
+//
+//  Created by Muzola Ethberg on 21/03/2025.
+//
+
+import Testing
+@testable import visitKongo
+
+struct visitKongoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
